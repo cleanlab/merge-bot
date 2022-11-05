7433c211eb458c619b9c4bef435e02093a7c7936
ryansingman <ryanjs@vt.edu>
2022-11-05 17:03:44 -0600

    exit quietly if checks do not pass, pt. 2

--- merge_bot/check_review_approval.py
+++ merge_bot/check_review_approval.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
             all_requested_reviewers_have_reviewed = False
 
     if not all_requested_reviewers_have_reviewed:
-        exit(1)
+        exit(0)
 
     # exit w/ non-zero code if not all reviewers approve
     if not all(reviewer_approvals):
